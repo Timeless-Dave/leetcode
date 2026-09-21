15d1d28977c20f5c35cd87f927378a6f4fcdf115
Timeless <havillahaod@gmail.com>
2026-09-21 00:13:04 -0500

    Time: 342 ms (78.95%) | Memory: 34.3 MB (52.63%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -1,18 +1,28 @@
 from typing import List
-class Solution:
-    def resultArray(self, nums: List[int], k: int) -> List[int]:
-        dp = [0]*k
-        result = [0]*k
-        for num in nums:
-            m = num % k
-            new_dp = [0] * k
-            for r in range(k):
 
-                cnt = dp[r]
-                if cnt:
-                    new_dp[(r*m) % k] += cnt
-            new_dp[m] += 1
-            dp = new_dp
-            for r in range(k):
-                result[r] += dp[r]
-        return result
+
+class Solution:
+
+  def resultArray(self, nums: List[int], k: int) -> List[int]:
+    dp = [0] * k
+    result = [0] * k
+
+    for num in nums:
+      m = num % k
+      new_dp = [0] * k
+
+      # Extend existing subarrays ending at the previous element
+      for r in range(k):
+        cnt = dp[r]
+        if cnt:
+          new_dp[(r * m) % k] += cnt
+
+      # Start a new subarray with the current element
+      new_dp[m] += 1
+      dp = new_dp
+
+      # Accumulate all subarrays ending at the current element
+      for r in range(k):
+        result[r] += dp[r]
+
+    return result
